6767b59e6015d879a838b7342895eeefead6103d
yonssaid <yonssaid@gmail.com>
2024-09-09 16:07:25 -0700

    Add DTOs for Admin and User Updates, Implement Admin Controller Methods, and Integrate Admin Tools
    - Added `RegisterAdminDto` for handling admin registration data, including fields for username, password, email, age, first name, last name, and role name.
    - Added `UpdateUserDto` for updating user information, with fields for username, email, age, first name, last name, and role name.
    - Updated `AdminController` to include new methods for handling admin-related operations:
      - Registration of new admin users.
      - Updating user information.
    - Integrated admin tools into the application:
      - Implemented `AdminTools` component within the app to provide access to admin functionalities.
      - Updated the user interface to reflect the new admin features and allow admin users to manage user information effectively.
    
    Commited by @Yonssaid

--- src/main/java/com/Tasker/Repositories/AdminRepository.java
+++ src/main/java/com/Tasker/Repositories/AdminRepository.java
@@ -4,7 +4,7 @@ import com.Tasker.Models.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Repository interface for managing {@link MyUser} entities.
+ * Repository interface for managing {@link MyUser} entities as Admin.
  * <p>
  * This interface extends {@link JpaRepository} to provide CRUD operations for {@link MyUser} entities.
  * </p>
